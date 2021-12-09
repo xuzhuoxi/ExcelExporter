45a4753a2a38d0d725b9b6ba954b82285be25542
xuzhuoxi <xuzhuoxi@gmail.com>
2021-12-09 10:34:49 +0800

    1.bug fix: []uint8类型转换数据错误。

--- Bug.md
+++ Bug.md
@@ -1,4 +1,5 @@
 1. 导出json数据是，错误地把[]uint8类型的数据识别为[]byte类型，因此导出的类型成为了string类型。
+1. 已经临时解决。
 
 2. yaml, toml, hcl, env, properties数据导出时，key为**大小写无关**，本意要求**大小写相关**。
 
--- src/core/data/builder_json.go
+++ src/core/data/builder_json.go
@@ -3,6 +3,7 @@ package data
 import (
 	"fmt"
 	"github.com/tidwall/sjson"
+	"github.com/xuzhuoxi/ExcelExporter/src/setting"
 	"io/fs"
 	"os"
 )
@@ -49,6 +50,9 @@ func (b *jsonDataBuilder) writeCell(ktv *KTValue) error {
 	if nil != err {
 		return err
 	}
+	if ktv.Type == setting.FieldUint8Arr {
+		v = b.unit8ArrToUint16Arr(v)
+	}
 	path := fmt.Sprintf("%s.%d.%s", dataName, b.rowIndex, ktv.Key)
 	c, err := sjson.Set(b.content, path, v)
 	if nil != err {
@@ -56,4 +60,19 @@ func (b *jsonDataBuilder) writeCell(ktv *KTValue) error {
 	}
 	b.content = c
 	return nil
+}
+
+func (b *jsonDataBuilder) unit8ArrToUint16Arr(uint8Arr interface{}) interface{} {
+	v := uint8Arr.([]uint8)
+	if nil == v {
+		return nil
+	}
+	if len(v) == 0 {
+		return []uint16{}
+	}
+	rs := make([]uint16, len(v))
+	for index := range v {
+		rs[index] = uint16(v[index])
+	}
+	return rs
 }
