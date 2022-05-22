9819c913d34ec81c405b7640f32f836cacf69afa
xuzhuoxi <xuzhuoxi@gmail.com>
2022-05-22 17:30:39 +0800

    1.更新版本为v1.0.1

--- build/goxc_build.bat
+++ build/goxc_build.bat
@@ -1,3 +1,3 @@
-goxc.exe -os="linux windows freebsd openbsd" -arch="amd64 arm" -n=ExcelExporter -pv=v1.0.0 -wd=../src -d=./release -include=*.go
+goxc.exe -os="linux windows freebsd openbsd" -arch="amd64 arm" -n=ExcelExporter -pv=v1.0.1 -wd=../src -d=./release -include=*.go
 
 pause
